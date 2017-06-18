120d4af266e94e4d291c5ecdc820e437c52665f4
dbm0204 <dbm0204@gmail.com>
2017-06-18 19:50:25 +0530

    Project Finished

--- app/src/main/java/com/example/dbm0204/assignment101/MainActivity.java
+++ app/src/main/java/com/example/dbm0204/assignment101/MainActivity.java
@@ -1,53 +1,77 @@
 package com.example.dbm0204.assignment101;
-
 import android.app.Activity;
 import android.app.Fragment;
-import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.content.pm.PackageManager;
-import android.content.res.Configuration;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
+
 public class MainActivity extends Activity {
-    private static EditText num;
-    private static TextView text;
+    int i = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // gets the configuration of the android system
-        Configuration config = getResources().getConfiguration();
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        /**
-         *  Check the orientation and act accordingly
-         */
-        if(config.orientation==Configuration.ORIENTATION_LANDSCAPE) {
-            /**
-             *  Landscape mode of the device
-             */
-            LM_Fragment ls_fragment = new LM_Fragment();
-            fragmentTransaction.replace(R.id.content,ls_fragment);
+
+        setContentView(R.layout.fragment_simple);
+        if (savedInstanceState == null) {
+            Fragment newFragment = SimpleAddition.newInstance(i);
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.add(R.id.FrameLayout1, newFragment).commit();
         } else {
-            /**
-             * Portrait Mode of the device
-             */
-            PM_Fragment  pm_fragment = new PM_Fragment();
-            fragmentTransaction.replace(R.id.content, pm_fragment);
-            fragmentTransaction.addToBackStack(null);
-
+            i = savedInstanceState.getInt("level");
         }
-        // Commit the transaction
-        fragmentTransaction.commit();
-
-        num = (EditText) findViewById(R.id.field);
-        String textString = num.getText().toString();
-        text= (TextView) findViewById(R.id.blank);
-        text.setText(textString);
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("level", i);
+    }
+
+    public static class SimpleAddition extends Fragment {
+        int mNum;
+
+        static SimpleAddition newInstance(int num) {
+            SimpleAddition f = new SimpleAddition();
+
+            Bundle args = new Bundle();
+            args.putInt("num", num);
+            f.setArguments(args);
+
+            return f;
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            mNum = getArguments() != null ? getArguments().getInt("num") : 1;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.activity_main, container, false);
+
+            Button b = (Button) v.findViewById(R.id.button1);
+            final EditText et1 = (EditText) v.findViewById(R.id.editText1);
+            final TextView tv = (TextView) v.findViewById(R.id.textView1);
+
+            b.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    tv.setText(et1.getText().toString());
+                }
+            });
+            return v;
+        }
+    }
 
 }
